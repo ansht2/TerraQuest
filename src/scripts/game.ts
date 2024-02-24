dd36820a71e4c8adb4e4b87737df25dd7366488b
atuls <atulsyan850@gmail.com>
2024-02-24 15:24:16 -0600

    init

---
+++ src/scripts/game.ts
@@ -0,0 +1,32 @@
+import 'phaser'
+import CountryScene from './scenes/countryScene'
+import PreloadScene from './scenes/preloadScene'
+import GameManager from "./objects/gameManager";
+
+const DEFAULT_WIDTH = 1280
+const DEFAULT_HEIGHT = 720
+
+const config = {
+  type: Phaser.AUTO,
+  backgroundColor: '#ffffff',
+  scale: {
+    parent: 'phaser-game',
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: DEFAULT_WIDTH,
+    height: DEFAULT_HEIGHT
+  },
+  scene: [PreloadScene, CountryScene],
+  physics: {
+    default: 'arcade',
+    arcade: {
+      debug: false,
+      gravity: { y: 400 }
+    }
+  }
+}
+
+window.addEventListener('load', () => {
+  const game = new Phaser.Game(config)
+  game['gameManager'] = new GameManager();
+})
---
+++ src/scripts/objects/gameManager.ts
@@ -0,0 +1,8 @@
+// GameManager.ts
+export default class GameManager {
+    // Properties and methods of your GameManager
+    score: number = 0;
+    country: { x: number; y: number } | null = null;
+    player_name: string | null = null;
+    // other properties and methods...
+}
